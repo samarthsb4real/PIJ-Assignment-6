c56b850a5e132caef00642883a43ee05815bb64a
Samarth Bhadane <samarthbhadane23@gmail.com>
2025-04-06 19:41:21 +0530

    Add Quack class implementing QuackBehaviour interface

---
+++ Quack.java
@@ -0,0 +1,6 @@
+// Quack class implementing QuackBehaviour interface
+public class Quack implements QuackBehaviour {
+    public void quack() {
+        System.out.println("Quack Quack!");
+    }
+}
