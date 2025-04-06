da07e8390364e3d639bd7fce9a75e94abe40a64e
Samarth Bhadane <samarthbhadane23@gmail.com>
2025-04-06 19:41:44 +0530

    Add Float class implementing SwimBehaviour interface

---
+++ Float.java
@@ -0,0 +1,6 @@
+// Float class that implements SwimBehaviour interface
+public class Float implements SwimBehaviour {
+    public void swim() {
+        System.out.println("I can float!");
+    }
+}
