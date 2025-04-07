f82caee40b3ec5b2e3085536118f4a5faf313943
Samarth Bhadane <samarthbhadane23@gmail.com>
2025-04-07 22:29:11 +0530

    Add FlyWithWings class implementing FlyBehaviour interface

---
+++ FlyWithWings.java
@@ -0,0 +1,6 @@
+// FlyWithWings class implementing FlyBehaviour interface
+public class FlyWithWings implements FlyBehaviour {
+    public void fly() {
+        System.out.println("I can fly with wings!");
+    }
+}
