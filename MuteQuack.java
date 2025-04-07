bbd6adc821c7834a8b5d9ba8445cf1e3397777ac
Samarth Bhadane <samarthbhadane23@gmail.com>
2025-04-07 22:29:19 +0530

    Add MuteQuack class implementing QuackBehaviour interface

---
+++ MuteQuack.java
@@ -0,0 +1,6 @@
+// MuteQuack class implementing QuackBehaviour interface
+public class MuteQuack implements QuackBehaviour {
+    public void quack() {
+        System.out.println("<< Silence >>");
+    }
+}
